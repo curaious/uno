2e177f5a49aaf1c51a7625aaf4181363315547cd
Praveen <praveen@amagi.com>
2026-01-16 17:34:14 +0530

    removed unused svc and apis

--- internal/services/services.go
+++ internal/services/services.go
@@ -5,15 +5,11 @@ import (
 
 	"github.com/curaious/uno/internal/config"
 	"github.com/curaious/uno/internal/db"
-	agent2 "github.com/curaious/uno/internal/services/agent"
 	agent_config2 "github.com/curaious/uno/internal/services/agent_config"
 	conversation2 "github.com/curaious/uno/internal/services/conversation"
-	mcp_server2 "github.com/curaious/uno/internal/services/mcp_server"
-	model2 "github.com/curaious/uno/internal/services/model"
 	project2 "github.com/curaious/uno/internal/services/project"
 	prompt2 "github.com/curaious/uno/internal/services/prompt"
 	provider2 "github.com/curaious/uno/internal/services/provider"
-	schema2 "github.com/curaious/uno/internal/services/schema"
 	traces2 "github.com/curaious/uno/internal/services/traces"
 	user2 "github.com/curaious/uno/internal/services/user"
 	virtual_key2 "github.com/curaious/uno/internal/services/virtual_key"
@@ -21,13 +17,9 @@ import (
 
 type Services struct {
 	Provider     *provider2.ProviderService
-	Model        *model2.ModelService
-	Agent        *agent2.AgentService
 	AgentConfig  *agent_config2.AgentConfigService
-	MCPServer    *mcp_server2.MCPServerService
 	Project      *project2.ProjectService
 	Prompt       *prompt2.PromptService
-	Schema       *schema2.SchemaService
 	Conversation *conversation2.ConversationService
 	VirtualKey   *virtual_key2.VirtualKeyService
 	Traces       *traces2.TracesService
@@ -60,11 +52,7 @@ func NewServices(conf *config.Config) *Services {
 		VirtualKey:   virtual_key2.NewVirtualKeyService(virtual_key2.NewVirtualKeyRepo(dbconn)),
 		Project:      project2.NewProjectService(project2.NewProjectRepo(dbconn)),
 		Prompt:       prompt2.NewPromptService(prompt2.NewPromptRepo(dbconn)),
-		Schema:       schema2.NewSchemaService(schema2.NewSchemaRepo(dbconn)),
-		Model:        model2.NewModelService(model2.NewModelRepo(dbconn)),
-		Agent:        agent2.NewAgentService(agent2.NewAgentRepo(dbconn)),
 		AgentConfig:  agent_config2.NewAgentConfigService(agent_config2.NewAgentConfigRepo(dbconn)),
-		MCPServer:    mcp_server2.NewMCPServerService(mcp_server2.NewMCPServerRepo(dbconn)),
 		Conversation: conversation2.NewConversationService(conversation2.NewConversationRepo(dbconn)),
 		Traces:       tracesSvc,
 		User:         user2.NewUserService(user2.NewUserRepo(dbconn)),
